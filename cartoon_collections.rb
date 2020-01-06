505f9f91cfe707bdf59bc2ebe7b847787017744a
Daniel Mayle <dmayle012@gmail.com>
2020-01-06 21:36:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves(array)
-  array.map
+  counter = 1 
+  array.each do |array|
+    puts "#{counter} #{array}"
+    counter += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
